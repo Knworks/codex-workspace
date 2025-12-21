ed01fcb10941556cea7ed591703647d08b1bd4c4
Knworks <kazukinoda@gmail.com>
2025-12-21 21:40:14 +0900

    feat(vscode): set extension and sidebar icons

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,25 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+class WorkspaceSidebarProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+	getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(): vscode.ProviderResult<vscode.TreeItem[]> {
+		return [];
+	}
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const treeDataProvider = new WorkspaceSidebarProvider();
+	const treeDataProviderDisposable = vscode.window.registerTreeDataProvider(
+		'codex-workspace.sidebar',
+		treeDataProvider,
+	);
+	context.subscriptions.push(treeDataProviderDisposable);
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,4 +1,6 @@
 import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -11,5 +13,27 @@ suite('Extension Test Suite', () => {
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	});
+
+	test('Package icons are configured', () => {
+		const packageJsonPath = path.resolve(__dirname, '..', '..', 'package.json');
+		const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+		assert.strictEqual(packageJson.icon, 'images/codex-workspace.png');
+
+		const activitybar = packageJson?.contributes?.viewsContainers?.activitybar;
+		assert.ok(Array.isArray(activitybar));
+		assert.ok(
+			activitybar.some(
+				(container: { id: string; icon: string }) =>
+					container.id === 'codex-workspace' && container.icon === 'images/sidebar.png',
+			),
+		);
+
+		const views = packageJson?.contributes?.views?.['codex-workspace'];
+		assert.ok(Array.isArray(views));
+		assert.ok(
+			views.some((view: { id: string }) => view.id === 'codex-workspace.sidebar'),
+		);
 	});
 });
