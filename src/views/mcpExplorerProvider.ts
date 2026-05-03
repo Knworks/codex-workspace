0f06788d550df0a54f50af9a43399e1088b3788c
Knworks <kazukinoda@gmail.com>
2026-05-03 23:31:18 +0900

    feat: use codicons for mcp explorer status icons

--- src/views/mcpExplorerProvider.ts
+++ src/views/mcpExplorerProvider.ts
@@ -1,16 +1,12 @@
 import * as vscode from 'vscode';
-import path from 'path';
 import { CodexTreeDataProvider } from './codexTreeProvider';
 import { CodexTreeItem } from '../models/treeItems';
 import { readMcpServers } from '../services/mcpService';
 import { resolveCodexPaths } from '../services/workspaceStatus';
 
 export class McpExplorerProvider extends CodexTreeDataProvider<CodexTreeItem> {
-	private readonly context: vscode.ExtensionContext;
-
-	constructor(context: vscode.ExtensionContext) {
+	constructor(_context: vscode.ExtensionContext) {
 		super();
-		this.context = context;
 	}
 
 	protected getAvailableChildren(): vscode.ProviderResult<CodexTreeItem[]> {
@@ -29,10 +25,12 @@ export class McpExplorerProvider extends CodexTreeDataProvider<CodexTreeItem> {
 				title: 'Toggle MCP',
 				arguments: [server.id],
 			};
-			const iconFileName = server.enabled ? 'status_run32.png' : 'status_run_grey32.png';
-			const iconPath = this.context.asAbsolutePath(path.join('images', iconFileName));
-			const iconUri = vscode.Uri.file(iconPath);
-			item.iconPath = { light: iconUri, dark: iconUri };
+			item.iconPath = server.enabled
+				? new vscode.ThemeIcon('mcp')
+				: new vscode.ThemeIcon(
+						'circle-slash',
+						new vscode.ThemeColor('disabledForeground'),
+					);
 			return item;
 		});
 	}
